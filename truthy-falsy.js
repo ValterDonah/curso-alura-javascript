605db221f2982affc5bba66a57a9993d2b6efacf
ValterDonah <valter.donah@gmail.com>
2023-01-15 15:15:05 -0300

    códigos var e conversões

---
+++ truthy-falsy.js
@@ -0,0 +1,30 @@
+// boolean
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy ou falsy
+
+// 0 = false
+// 1 = true
+// "" = false
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+console.log(1 == false);
+
+// null -> vazio ou nada
+// undefined
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
---
+++ var-let-const.js
@@ -0,0 +1,41 @@
+// var
+
+// var altura = 5;
+// var comprimento = 7;
+// var area;
+
+// area = altura * comprimento;
+
+// console.log(area);
+
+
+// let
+
+// let forma = "retângul";
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+// if (forma === "retângulo") {
+//     area = altura * comprimento;
+//     console.log("A área do retângulo é: " + area);
+// } else {
+//     area = (altura * comprimento) / 2;
+//     console.log("A área do triângulo é: " + area);
+// }
+
+
+// const
+
+const forma = "retângulo";
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === "retângulo") {
+    area = altura * comprimento;
+    console.log("A área do retângulo é: " + area);
+} else {
+    area = (altura * comprimento) / 2;
+    console.log("A área do triângulo é: " + area);
+}
